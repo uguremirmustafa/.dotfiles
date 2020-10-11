4aca98d7a2bc5be5c684b797ee7af4acdbd67337
uguremirmustafa <uguremirmustafa@gmail.com>
2020-10-11 23:05:11 +0300

    i3 config added

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,4 +9,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+# polybar main -c ~/.config/polybar/config.ini &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c ~/.config/polybar/config.ini &
+  done
+else
+  polybar --reload main -c ~/.config/polybar/config.ini &
+fi
